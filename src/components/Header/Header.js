71a24440226e39898a8fe2ab63c464fea89babb9
Marina Natalina <kiska21mk@yandex.ru>
2022-07-28 10:58:38 +0300

    роуты

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <header>
+        <Link to='/'>Логотип</Link>
+        <Link to='/movies'>Фильмы</Link>
+        <Link to='/saved-movies'>Сохранённые фильмы</Link>
+        <Link to='/signup'>Регистрация</Link>
+        <Link to='/signin'>Авторизация</Link>
+        <Link to='/profile'>Аккаунт</Link>
+      </header>
+    </>
+
   )
 }
 
