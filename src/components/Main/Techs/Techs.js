1f2c40a7e02249203ff478a2543df16114c42bf8
Marina Natalina <kiska21mk@yandex.ru>
2022-07-31 12:06:52 +0300

    компонент технологии

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -3,7 +3,22 @@ import './Techs.css';
 
 const Techs = () => {
   return (
-    <div>Techs</div>
+    <section id='tachs' className='techs'>
+      <div className='techs__content'>
+        <h3 className='techs__title'>Технологии</h3>
+        <h2 className='techs__subtitle'>7 технологий</h2>
+        <p className='techs__text'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+      </div>
+      <ul className='techs-list'>
+        <li className='techs-list__item'>HTML</li>
+        <li className='techs-list__item'>CSS</li>
+        <li className='techs-list__item'>JS</li>
+        <li className='techs-list__item'>React</li>
+        <li className='techs-list__item'>Git</li>
+        <li className='techs-list__item'>Express.js</li>
+        <li className='techs-list__item'>mongoDB</li>
+      </ul>
+    </section>
   )
 }
 
