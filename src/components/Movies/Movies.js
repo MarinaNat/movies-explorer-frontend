d0655a3cc3ab67a10ea880cfb392ea7d0ccb7690
Marina Natalina <kiska21mk@yandex.ru>
2022-08-01 20:54:34 +0300

    компонент SearchForm

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Movies.css';
+import SearchForm from './SearchForm/SearchForm.js';
+import MoviesCardList from './MoviesCardList/MoviesCardList.js';
 
 const Movies = () => {
   return (
-    <div>Movies</div>
-  )
+  <main className='movies'>
+<SearchForm />
+<MoviesCardList />
+<button className='link_button movies__more-button'>Ещё</button>
+  </main>  
+)
 }
 
 export default Movies
--- src/components/Movies/Preloader/Preloader.js
+++ src/components/Movies/Preloader/Preloader.js
@@ -3,8 +3,12 @@ import './Preloader.css';
 
 const Preloader = () => {
   return (
-    <div>Preloader</div>
-  )
+    <div className="preloader">
+        <div className="preloader__container">
+            <span className="preloader__round"></span>
+        </div>
+    </div>
+)
 }
 
 export default Preloader
